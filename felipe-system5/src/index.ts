04a0726162b0af3165d802404ad5fa50db8a6b43
FelipeSouza <united.fss@gmail.com>
2022-09-12 10:42:19 -0300

    resolucao projeto system5 felipe

---
+++ felipe-system5/src/index.ts
@@ -0,0 +1,23 @@
+import { app } from "./app";
+import DocenteEndpoint from "./endpoints/Docente";
+import EstudanteEndpoint from "./endpoints/Estudante";
+import Estudante from "./endpoints/Estudante";
+import TurmaEndpoint from "./endpoints/Turma";
+
+
+const turma = new TurmaEndpoint()
+const estudante = new EstudanteEndpoint()
+const docente = new DocenteEndpoint()
+
+app.post("/criar-turma",turma.criar)
+app.get("/buscar-turmas-ativas",turma.ativa)
+app.post("/mudar-modulo/:id",turma.modulo)
+
+app.post("/criar-estudante",estudante.criar)
+app.get("/estudante/:nome",estudante.buscar)
+app.post("/mudar-estudante/:id",estudante.mudarTurma)
+
+app.post("/criar-docente",docente.criar)
+app.get("/buscar-docentes",docente.buscarTodos)
+app.post("/mudar-docente-turma/:id",docente.mudarTurma)
+
